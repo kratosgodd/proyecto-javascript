378fad16e10b70beca4c505e8bc8103d5040cb52
kratosgodd <franulibarri1@gmail.com>
2023-11-29 23:42:30 -0300

    primer commit

---
+++ js/index.js
@@ -0,0 +1,29 @@
+console.log("hi wordl");
+
+let surname;
+let telephone;
+
+surname = "Perez";
+console.log(surname);
+
+telephone = 1234
+console.log(telephone);
+
+let country = "Argentina";
+let age = 21;
+
+console.log(country);
+console.log(age);
+
+var fruit = "banana"
+console.log(fruit);
+
+fruit = "apple"
+console.log(fruit);
+
+const greeting = "hi"
+console.log(greeting);
+
+
+
+
